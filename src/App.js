8e6cddabd244ad8f0aa20470ff52ae0738d4317a
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-01-16 23:40:57 -0600

    Create todo-list and individual todos. Save todos in local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos])
+
+  function toggleTodo(id) {
+    // copy of our current todos
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo(event) {
+    // 1. take previous todos
+    // 2. add new todo
+    // 3. set todos to that todo list
+
+    // need access to the input name
+    // use ref hook
+    const name = todoNameRef.current.value;
+    if (name === '') return 
+    console.log('add to do: ' + name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    // clear the input 
+    todoNameRef.current.value = null;
+  }
   return (
-    <TodoList />
-    
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>add to do</button>
+      <button>clear completed todos</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   )
 }
 
